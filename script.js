fcb196351219c0019fed4946ab2c729942e2ecf7
paiva737 <rafaelpaiva636@gmail.com>
2024-08-12 20:30:25 -0300

    refazendo alteracoes

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
+    img.setAttribute("src", "./imagens/fotoperfil2.jpeg")
+  } else {
     img.setAttribute("src", "./imagens/fotoperfil.jpeg")
-  } 
+  }
 }
