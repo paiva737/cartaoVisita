fb8951bcf938b59100002b2c1511a376fa7a3885
paiva737 <rafaelpaiva636@gmail.com>
2024-08-12 20:20:25 -0300

    alteracao de planos de fundos

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./imagens/fotoperfil2.jpeg")
-  } else {
     img.setAttribute("src", "./imagens/fotoperfil.jpeg")
-  }
+  } 
 }
